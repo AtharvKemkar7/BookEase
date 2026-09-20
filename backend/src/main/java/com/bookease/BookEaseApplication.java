30a278373b90dc435fee7244c2dcfc79da3da0a0
Atharv Kemkar <atharvkemkar7@gmail.com>
2026-09-20 22:26:21 +0530

    added

---
+++ backend/src/main/java/com/bookease/BookEaseApplication.java
@@ -0,0 +1,12 @@
+package com.bookease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookEaseApplication.class, args);
+    }
+}
